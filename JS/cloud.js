0b88e2f9bde5d617e0494a9a819c0d93acbff258
BoroBilobrk <bilobrk.boro@gmail.com>
2025-12-07 22:25:07 +0100

    Expose cloud-related functions to the global window object

--- JS/cloud.js
+++ JS/cloud.js
@@ -1,1 +1,4 @@
-// Placeholder cloud
+window.renderCloudFiles = renderCloudFiles;
+window.saveToCloud = saveToCloud;
+window.loadArchive = loadArchive;
+window.openPdfFromCloud = openPdfFromCloud;// Placeholder cloud
