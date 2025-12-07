c88a94b683a512e25062879f539211fe12020bda
BoroBilobrk <bilobrk.boro@gmail.com>
2025-12-07 22:31:59 +0100

    Implement placeholder startOCR function
    Added a placeholder OCR function with an alert.

--- JS/ocr.js
+++ JS/ocr.js
@@ -1,1 +1,8 @@
-window.startOCR = startOCR;// Placeholder ocr
+// OCR – postavljanje temelja za kasniju verziju
+
+async function startOCR() {
+    alert("OCR funkcija je u pripremi.");
+}
+
+// GLOBAL EXPORT
+window.startOCR = startOCR;
