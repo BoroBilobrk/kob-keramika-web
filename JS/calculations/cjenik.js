8dce53e0f11c827af8cabecea02c411e8edd6a46
BoroBilobrk <bilobrk.boro@gmail.com>
2025-12-10 21:21:34 +0100

    Add UNIT_PRICES constant for unit pricing

---
+++ JS/calculations/cjenik.js
@@ -0,0 +1,12 @@
+// js/calculations/cjenik.js
+export const UNIT_PRICES = {
+  pod: 10,
+  zidovi: 12,
+  hidroPod: 7,
+  hidroTus: 9,
+  hidroTraka: 2,
+  silikon: 1.5,
+  sokl: 4,
+  lajsne: 5,
+  gerung: 8
+};
