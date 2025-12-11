78fea7cd99ef31b16d44876278775c3685fc35a3
BoroBilobrk <bilobrk.boro@gmail.com>
2025-12-11 14:11:15 +0100

    Rename state to AppState in state.js

--- JS/core/state.js
+++ JS/core/state.js
@@ -1,6 +1,7 @@
-// js/core/state.js
-export const state = {
-  siteRooms: [],
-  openings: [],
-  lastCalc: null
+// JS/core/state.js
+
+export const AppState = {
+  siteRooms: [],   // sve prostorije na gradilištu
+  openings: [],    // otvori za trenutnu prostoriju
+  lastCalc: null,  // zadnji izračun (za PDF/CSV)
 };
