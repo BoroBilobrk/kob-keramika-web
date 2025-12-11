f76b3c9642fa44d551325390646c9bcdd8de8f5a
BoroBilobrk <bilobrk.boro@gmail.com>
2025-12-11 14:12:22 +0100

    Initialize unit prices to 0 and add input reading
    Updated unit prices to initialize at 0 and added functions to read prices from input fields.

--- JS/calculations/cjenik.js
+++ JS/calculations/cjenik.js
@@ -1,12 +1,31 @@
-// js/calculations/cjenik.js
+// JS/calculations/cjenik.js
+import { $, parseNum } from "../core/helpers.js";
+
+// ručni cjenik – vrijednosti se pune iz input polja
 export const UNIT_PRICES = {
-  pod: 10,
-  zidovi: 12,
-  hidroPod: 7,
-  hidroTus: 9,
-  hidroTraka: 2,
-  silikon: 1.5,
-  sokl: 4,
-  lajsne: 5,
-  gerung: 8
+  pod: 0,
+  zidovi: 0,
+  hidroPod: 0,
+  hidroTus: 0,
+  hidroTraka: 0,
+  silikon: 0,
+  sokl: 0,
+  lajsne: 0,
+  gerung: 0
 };
+
+export function readPricesFromInputs() {
+  UNIT_PRICES.pod        = parseNum($("#pricePod")?.value);
+  UNIT_PRICES.zidovi     = parseNum($("#priceZidovi")?.value);
+  UNIT_PRICES.hidroPod   = parseNum($("#priceHidroPod")?.value);
+  UNIT_PRICES.hidroTus   = parseNum($("#priceHidroTus")?.value);
+  UNIT_PRICES.hidroTraka = parseNum($("#priceHidroTraka")?.value);
+  UNIT_PRICES.silikon    = parseNum($("#priceSilikon")?.value);
+  UNIT_PRICES.sokl       = parseNum($("#priceSokl")?.value);
+  UNIT_PRICES.lajsne     = parseNum($("#priceLajsne")?.value);
+  UNIT_PRICES.gerung     = parseNum($("#priceGerung")?.value);
+}
+
+export function savePrices() {
+  readPricesFromInputs();
+}
