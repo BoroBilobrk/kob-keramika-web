95f3c362e196ef92000869d66da42e3c813b15a5
BoroBilobrk <bilobrk.boro@gmail.com>
2025-12-12 13:59:24 +0100

    Add console log to indicate app.js loading

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,6 @@
 // JS/app.js – glavni modul (minimalna verzija)
 // učitava samo UI, a ui.js dalje učitava sve ostale module
+console.log("APP.JS LOADED!");
+import "./core/ui.js";
 
 import "./core/ui.js";
