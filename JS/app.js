b6d8c6a10483989062621314f73bbf0a4ca8fd66
BoroBilobrk <bilobrk.boro@gmail.com>
2025-12-12 14:24:38 +0100

    Clean up comments and import statement

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,3 @@
-// JS/app.js – glavni modul (minimalna verzija)
-// učitava samo UI, a ui.js dalje učitava sve ostale module
+// JS/app.js – glavni modul
 console.log("APP.JS LOADED!");
-import "./core/ui.js";
-
 import "./core/ui.js";
