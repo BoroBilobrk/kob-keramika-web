2d27341ebed663196744c4790af38ee2bb9c927b
BoroBilobrk <bilobrk.boro@gmail.com>
2025-12-19 11:58:54 +0100

    Update import statements and console log message

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,5 @@
 // JS/app.js – glavni modul
-console.log("APP.JS LOADED!");
-import "./core/ui.js?v=2";
+console.log("APP.JS LOADED");
+
+import "./core/ui.js";
+import "./calculations/autoCalc.js";
