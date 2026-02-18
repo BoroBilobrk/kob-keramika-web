ed7c01687136d293a425d8c647cf13ca084f1974
BoroBilobrk <bilobrk.boro@gmail.com>
2026-02-18 13:39:10 +0100

    Update pdfBase.js with improved header containing company data, logo, and site information.

---
+++ pdfBase.js
@@ -0,0 +1,9 @@
+// Improved header for pdfBase.js
+/**
+ * Company: Your Company Name
+ * Logo: ![Company Logo](https://link_to_logo)
+ * Website: https://yourcompanywebsite.com
+ * Updated on: 2026-02-18 12:38:56 UTC
+ */
+
+// Remaining code for pdfBase.js
