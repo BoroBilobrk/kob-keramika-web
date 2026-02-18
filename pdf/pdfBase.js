ed73f534c11e8440634b7c72d3817d0aee98c448
BoroBilobrk <bilobrk.boro@gmail.com>
2026-02-18 13:41:15 +0100

    Updated header with company data

---
+++ pdf/pdfBase.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Company Information
+// KOB-KERAMIKA
+// Address: Tina Ujevića 4
+// OIB: 27080482187
+// IBAN: HR0823600001102094050
+// SWIFT: ZABAHR2X
+
+// Rest of the JS code goes here ...
